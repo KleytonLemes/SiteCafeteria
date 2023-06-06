1aefb92d20b7fefd499d3a3e79aaed59e501c3a6
KleytonLemes <127805157+KleytonLemes@users.noreply.github.com>
2023-06-06 14:50:04 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("relogio").innerText = time;
+    document.getElementById("relogio").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
+
+
+let barraNav = document.querySelector('.barraMenu');
+
+var validar = document.getElementById('bt_pesquisar')
+var pesquisaForm = document.querySelector('.pesquisarFormulario')
+pesquisaForm.style.display = 'none';
+
+validar.addEventListener('click', function(){
+  if (pesquisaForm.style.display === 'block') {
+      pesquisaForm.style.display = 'none';
+  } else {
+      pesquisaForm.style.display = 'block';
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
